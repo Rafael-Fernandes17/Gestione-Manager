ee9265d7fa62dc9b41c18adbc41e119cfe272784
Rafael Fernandes <rafael.botelho.fernandes16@gmail.com>
2026-04-12 01:38:59 -0300

    Adicionando nav bar, ajustando verificacoes de cadastro e ajustando layout

--- php/cadastrarFuncionario.php
+++ php/cadastrarFuncionario.php
@@ -1,6 +1,8 @@
 <?php
     include_once('conexao.php');
 
+    header('Content-Type: application/json; charset=utf-8');
+
     $retorno = [
         'status' => '',
         'mensagem' => '',
@@ -10,13 +12,24 @@
 $nome = $_POST['nome'] ?? '';
 $email = $_POST['email'] ?? '';
 $senha = $_POST['senha'] ?? '';
+$eAdm = isset($_POST['eAdm']) ?? '';
 
-if (empty($nome) || empty($email) || empty($senha)) {
-    die("Preencha todos os campos.");
+if (empty($nome) || empty($email) || empty($senha) || empty($eAdm)) {
+    $retorno = [
+        'status' => 'nok', 
+        'mensagem' => 'Preencha todos os campos'
+        ];
+    echo json_encode($retorno);
+    exit;
 }
 
 if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    die("E-mail inválido.");
+    $retorno = [
+        'status' => 'nok', 
+        'mensagem' => 'Email invalido'
+        ];
+    echo json_encode($retorno);
+    exit;
 }
 
 $senha_hash = password_hash($senha, PASSWORD_DEFAULT);
@@ -28,14 +41,20 @@ $checandoEmail->execute();
 $resultadoDaConsulta = $checandoEmail->get_result();
 
 if ($resultadoDaConsulta->num_rows > 0) {
-    die("E-mail já cadastrado.");
-    $checandoEmail->close();
+        $retorno = [ 
+            'status' => 'nok',
+            'mensagem' => 'email repetido',
+            'data' => []
+            ];
+        echo json_encode($retorno);
+        $checandoEmail->close();
+        exit;
 } else {
     $checandoEmail->close();
 
     // Inserir usuário
-    $stmt = $conexao->prepare("INSERT INTO funcionario (nome, email, senha) VALUES (?, ?, ?)");
-    $stmt->bind_param("sss", $nome, $email, $senha_hash);
+    $stmt = $conexao->prepare("INSERT INTO funcionario (nome, email, senha, eAdm) VALUES (?, ?, ?, ?)");
+    $stmt->bind_param("sssi", $nome, $email, $senha_hash, $eAdm);
 
     if ($stmt->execute()) {
         $retorno = [
@@ -48,12 +67,10 @@ if ($resultadoDaConsulta->num_rows > 0) {
             'mensagem' => 'cadastro nao efetuado'
         ];
     }
+    $stmt->close();
 }
 
-
-$stmt->close();
 $conexao->close();
-header('Content-type: application/json; charset=utf-8');
 echo json_encode($retorno);
 
 ?>
