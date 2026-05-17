96a1b7d86f36d03cbb7a79ae3b1c4fa481d286cf
Rafael Fernandes <rafael.botelho.fernandes16@gmail.com>
2026-05-16 22:48:00 -0300

    removendo 'filtraSessao.js' do indexFuncionario.php

--- indexFuncionario.php
+++ indexFuncionario.php
@@ -10,7 +10,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/reset.css">
     <link rel="stylesheet" href="./css/cadastrarFuncionario.css">
-    <script src='js/filtraSessao.js' defer></script>
     <title>Document</title>
 </head>
 <body>
